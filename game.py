3723ceb7bbfc7b3c7ddabe027ef31f8f27767bd3
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-02-12 13:39:12 +0100

    fix

--- game.py
+++ game.py
@@ -1,12 +1,17 @@
-from numpy import stack, zeros, float32
+from numpy import zeros, float32
 from torch import tensor
 
 
 class Game:
-    def __init__(self) -> None:
+    def __init__(self, n=1) -> None:
         super().__init__()
-        self._board = zeros((3, 10, 10), dtype=float32)
+        self.n = n
+        self._board = zeros((n, 3, 10, 10), dtype=float32)
+
+    def change(self):
+        for i in range(self.n):
+            self._board[i, :, :, :] = zeros((3, 10, 10), dtype=float32) + 1
 
     @property
     def board(self):
-        return tensor(stack([self._board]))
+        return tensor(self._board)
