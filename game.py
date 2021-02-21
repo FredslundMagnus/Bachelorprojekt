673bbe72d5bed718f386f281e49051dec1b8f400
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-02-21 21:02:48 +0100

    Fix

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ class Game:
         super().__init__()
         self.batch: int = batch
         self.hours: float = hours
-        self.layers: Layers = Layers(batch, width, height, LayerType.Blocks, LayerType.Keys, LayerType.Door, LayerType.Holder, LayerType.Goal)
+        self.layers: Layers = Layers(batch, width, height, *LayerType)
         self.layers.update()
 
     @property
