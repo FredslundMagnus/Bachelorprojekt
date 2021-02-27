2c50c193cc50988d12d55822a185b127b3ce1d4c
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-02-27 12:58:25 +0100

    experiments

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -3,4 +3,4 @@ mkdir ../outputs/test_run/
 mkdir ../outputs/test_run/Markdown
 mkdir ../outputs/test_run/Agents
 mkdir ../outputs/test_run/Collectors
-bsub -o "../outputs/test_run/Markdown/test_run_0.md" -J "test_run_0" -P "-name test_run-0 -gamma 1 -network Networks.Small -learner Learners.DoubleQlearn -hours 5.1" < submit.sh
+bsub -o "../outputs/test_run/Markdown/test_run_0.md" -J "test_run_0" -P "-name test_run-0 -gamma 1 -network Networks.Small -learner Learners.DoubleQlearn -hours 0.2" < submit.sh
