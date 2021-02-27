6b2acf00236832ee94c8a73448b6a1728c2a65b3
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-02-27 13:22:44 +0100

    Fix

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-mkdir ../outputs/test_run_2/
-mkdir ../outputs/test_run_2/Markdown
-mkdir ../outputs/test_run_2/Agents
-mkdir ../outputs/test_run_2/Collectors
-bsub -o "../outputs/test_run_2/Markdown/test_run_2_0.md" -J "test_run_2_0" -P "-name test_run_2-0 -gamma 1 -network Networks.Small -learner Learners.DoubleQlearn -hours 0.2" < submit.sh
+mkdir ../outputs/test_run_3/
+mkdir ../outputs/test_run_3/Markdown
+mkdir ../outputs/test_run_3/Agents
+mkdir ../outputs/test_run_3/Collectors
+bsub -o "../outputs/test_run_3/Markdown/test_run_3_0.md" -J "test_run_3_0" -P "-name test_run_3-0 -gamma 1 -network Networks.Small -learner Learners.DoubleQlearn -hours 0.2" < submit.sh
