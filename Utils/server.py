68adfe82d3ab69bf9f3f87e9d694af3c3f55c240
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-03-01 16:23:36 +0100

    Fix

--- Utils/server.py
+++ Utils/server.py
@@ -1,6 +1,5 @@
 
 from Utils.debug import checkServer, profilingStats, showParams
-import pickle
 from typing import List
 import sys
 from network import Networks
@@ -28,20 +27,3 @@ def getvals(defaults):
 def serverRun(params):
     showParams(params)
     profilingStats()
-
-
-# def saveAgent(agent, name: str):
-#     agent.memory.reset(0)
-#     agent.memory = None
-#     agent.remember = None
-#     if isServer:
-#         pickle.dump(agent, open(f"outputs/{'-'.join(name.split('-')[:-1])}/Agents/{name}.agent", "wb"))
-#     else:
-#         pickle.dump(agent, open(f"trainlocally/{'-'.join(name.split('-')[:-1])}/{name}", "wb"))
-
-
-# def saveCollector(collector, name: str):
-#     if isServer:
-#         pickle.dump(collector, open(f"outputs/{'-'.join(name.split('-')[:-1])}/Collectors/{name}.collect", "wb"))
-#     else:
-#         pickle.dump(collector, open(f"trainlocally/{'-'.join(name.split('-')[:-1])}/collect_{name}", "wb"))
