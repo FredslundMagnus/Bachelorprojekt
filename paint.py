6b7e18f2a0de0506300808fe96bf61056d4b1f34
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-03-03 20:41:57 +0100

    Fix

--- paint.py
+++ paint.py
@@ -41,8 +41,7 @@ class Paint:
             Paint.write(f"Frames: {frame}", game.layers.width/2, 0)
             Paint.write(f"Game: {Paint.dim}", game.layers.width/2, game.layers.height-1)
             if teleporter != None:
-                val = int(teleporter.current_interventions[Paint.dim])
-                y, x = divmod(val, game.layers.width)
+                y, x = divmod(int(teleporter.current_interventions[Paint.dim]), game.layers.width)
                 Paint.drawRect(Colors.indigo.transparrent(180), size, x, y)
 
     @staticmethod
