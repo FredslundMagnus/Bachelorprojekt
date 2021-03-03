06f126785256081f12967973569d0a03d79434e5
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-03-03 13:02:28 +0100

    Fix

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from agent import Teleport_intervention, Mover, Networks, Learners, Explorations
 from collector import Collector
 from auxillaries import run, loop, person, random
 from save import Save
+from helper import function
+
+
+def teleport(defaults):
+    collector = Collector()
+    env = Game(**defaults)
+    mover = Mover(env, **defaults)
+    teleporter = Teleport_intervention(env, **defaults)
+
+    with Save(collector, mover, **defaults) as save:
+        for frame in loop(env, collector, save):
+            modified_board, intervention = teleporter(env.board)
+            actions = mover(modified_board)  # actions = mover(env.board)
+            observations, rewards, dones = env.step(actions)
+            modified_observations, modified_rewards, modified_dones = teleporter.modify(intervention, observations, rewards, dones)
+            mover.learn(modified_observations, actions, modified_rewards, modified_dones)  # mover.learn(observations, actions, rewards, dones)
+            teleporter.learn(observations, intervention, rewards, dones)
+            collector.collect(rewards, dones)
 
 
 class Defaults:
@@ -21,23 +39,7 @@ class Defaults:
     height: int = 11
     update: int = 1000
     reset_chance: float = 0.002
+    main: function = teleport
 
 
-def main(defaults):
-    collector = Collector()
-    env = Game(**defaults)
-    mover = Mover(env, **defaults)
-    teleporter = Teleport_intervention(env, **defaults)
-
-    with Save(collector, mover, **defaults) as save:
-        for frame in loop(env, collector, save):
-            modified_board, intervention = teleporter(env.board)
-            actions = mover(modified_board) # actions = mover(env.board)
-            observations, rewards, dones = env.step(actions)
-            modified_observations, modified_rewards, modified_dones = teleporter.modify(intervention, observations, rewards, dones)
-            mover.learn(modified_observations, actions, modified_rewards, modified_dones) # mover.learn(observations, actions, rewards, dones)
-            teleporter.learn(observations, intervention, rewards, dones)
-            collector.collect(rewards, dones)
-
-
-run(Defaults, main)
+run(Defaults)
