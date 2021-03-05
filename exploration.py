7762c444e4fba9430c7b08ea57b266e68de26ad9
TheDingodile <TheDingodile@gmail.com>
2021-03-05 13:44:12 +0100

    fixed_

--- exploration.py
+++ exploration.py
@@ -33,7 +33,7 @@ class Exploration:
 
     @property
     def K_(self):
-        return max(0.05, self.K / (5 * self.counter))
+        return max(0.05, self.K / (10 * self.counter))
 
     def greedy(self, vals):
         vals.detach()
