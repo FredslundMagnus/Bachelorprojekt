df100cedd9dc92687e60173f938d266f19c93599
TheDingodile <TheDingodile@gmail.com>
2021-03-05 02:26:40 +0100

    lets_go_test

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ class Defaults:
     exploration2: Explorations = Explorations.epsilonGreedy
     replay_buffer: replay_buffer = replay_buffer
     gamma: float = 0.95
-    K: float = 250000
+    K: float = 500000
     batch: int = 100
     hours: float = 12.0
     width: int = 11
