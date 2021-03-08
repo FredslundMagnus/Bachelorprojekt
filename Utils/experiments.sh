00c89ad6e0c422be8a91e8e006cd779022c00795
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-03-09 00:57:31 +0100

    Fix

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
-mkdir ../outputs/keys_door_small/
-mkdir ../outputs/keys_door_small/Markdown
-bsub -o "../outputs/keys_door_small/Markdown/keys_door_small_0.md" -J "keys_door_small_0" -P "-name keys_door_small-0 -hours 10.0 -width 9 -height 9 -layer_Keys True -layer_Door True" < submit.sh
-mkdir ../outputs/holder_putter_small/
-mkdir ../outputs/holder_putter_small/Markdown
-bsub -o "../outputs/holder_putter_small/Markdown/holder_putter_small_0.md" -J "holder_putter_small_0" -P "-name holder_putter_small-0 -hours 10.0 -width 9 -height 9 -layer_Holder True -layer_Putter True" < submit.sh
-mkdir ../outputs/keys_door_gold_small/
-mkdir ../outputs/keys_door_gold_small/Markdown
-bsub -o "../outputs/keys_door_gold_small/Markdown/keys_door_gold_small_0.md" -J "keys_door_gold_small_0" -P "-name keys_door_gold_small-0 -hours 10.0 -width 9 -height 9 -layer_Keys True -layer_Door True -layer_Gold True" < submit.sh
-mkdir ../outputs/holder_putter_gold_small/
-mkdir ../outputs/holder_putter_gold_small/Markdown
-bsub -o "../outputs/holder_putter_gold_small/Markdown/holder_putter_gold_small_0.md" -J "holder_putter_gold_small_0" -P "-name holder_putter_gold_small-0 -hours 10.0 -width 9 -height 9 -layer_Holder True -layer_Putter True -layer_Gold True" < submit.sh
-mkdir ../outputs/keys_door_holder_putter_gold_small/
-mkdir ../outputs/keys_door_holder_putter_gold_small/Markdown
-bsub -o "../outputs/keys_door_holder_putter_gold_small/Markdown/keys_door_holder_putter_gold_small_0.md" -J "keys_door_holder_putter_gold_small_0" -P "-name keys_door_holder_putter_gold_small-0 -hours 10.0 -width 9 -height 9 -layer_Keys True -layer_Door True -layer_Holder True -layer_Putter True -layer_Gold True" < submit.sh
+mkdir ../outputs/9x9_K_100000/
+mkdir ../outputs/9x9_K_100000/Markdown
+bsub -o "../outputs/9x9_K_100000/Markdown/9x9_K_100000_0.md" -J "9x9_K_100000_0" -P "-name 9x9_K_100000-0 -hours 10.0 -width 9 -height 9 -layer_Keys True -layer_Door True -layer_Holder True -layer_Putter True -layer_Gold True -K 100000.0" < submit.sh
+mkdir ../outputs/9x9_K_500000/
+mkdir ../outputs/9x9_K_500000/Markdown
+bsub -o "../outputs/9x9_K_500000/Markdown/9x9_K_500000_0.md" -J "9x9_K_500000_0" -P "-name 9x9_K_500000-0 -hours 10.0 -width 9 -height 9 -layer_Keys True -layer_Door True -layer_Holder True -layer_Putter True -layer_Gold True -K 500000.0" < submit.sh
+mkdir ../outputs/9x9_K_100000_epsilon_cap_0.1/
+mkdir ../outputs/9x9_K_100000_epsilon_cap_0.1/Markdown
+bsub -o "../outputs/9x9_K_100000_epsilon_cap_0.1/Markdown/9x9_K_100000_epsilon_cap_0.1_0.md" -J "9x9_K_100000_epsilon_cap_0.1_0" -P "-name 9x9_K_100000_epsilon_cap_0.1-0 -hours 10.0 -width 9 -height 9 -layer_Keys True -layer_Door True -layer_Holder True -layer_Putter True -layer_Gold True -K 100000.0 -epsilon_cap 0.1" < submit.sh
+mkdir ../outputs/9x9_K_500000_epsilon_cap_0.1/
+mkdir ../outputs/9x9_K_500000_epsilon_cap_0.1/Markdown
+bsub -o "../outputs/9x9_K_500000_epsilon_cap_0.1/Markdown/9x9_K_500000_epsilon_cap_0.1_0.md" -J "9x9_K_500000_epsilon_cap_0.1_0" -P "-name 9x9_K_500000_epsilon_cap_0.1-0 -hours 10.0 -width 9 -height 9 -layer_Keys True -layer_Door True -layer_Holder True -layer_Putter True -layer_Gold True -K 500000.0 -epsilon_cap 0.1" < submit.sh
+mkdir ../outputs/9x9_K_100000_epsilon_cap_0/
+mkdir ../outputs/9x9_K_100000_epsilon_cap_0/Markdown
+bsub -o "../outputs/9x9_K_100000_epsilon_cap_0/Markdown/9x9_K_100000_epsilon_cap_0_0.md" -J "9x9_K_100000_epsilon_cap_0_0" -P "-name 9x9_K_100000_epsilon_cap_0-0 -hours 10.0 -width 9 -height 9 -layer_Keys True -layer_Door True -layer_Holder True -layer_Putter True -layer_Gold True -K 100000.0 -epsilon_cap 0.0" < submit.sh
+mkdir ../outputs/9x9_K_500000_epsilon_cap_0/
+mkdir ../outputs/9x9_K_500000_epsilon_cap_0/Markdown
+bsub -o "../outputs/9x9_K_500000_epsilon_cap_0/Markdown/9x9_K_500000_epsilon_cap_0_0.md" -J "9x9_K_500000_epsilon_cap_0_0" -P "-name 9x9_K_500000_epsilon_cap_0-0 -hours 10.0 -width 9 -height 9 -layer_Keys True -layer_Door True -layer_Holder True -layer_Putter True -layer_Gold True -K 500000.0 -epsilon_cap 0.0" < submit.sh
