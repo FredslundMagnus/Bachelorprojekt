63ef8f6c79abe15a28b302eea0e0e53c34d5d584
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-03-18 19:54:37 +0100

    Fix

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-mkdir ../outputs/gold_9x9/
-mkdir ../outputs/gold_9x9/Markdown
-bsub -o "../outputs/gold_9x9/Markdown/gold_9x9_0.md" -J "gold_9x9_0" -P "-name gold_9x9-0 -hours 24.0 -width 9 -height 9" < submit.sh
-bsub -o "../outputs/gold_9x9/Markdown/gold_9x9_1.md" -J "gold_9x9_1" -P "-name gold_9x9-1 -hours 24.0 -width 9 -height 9" < submit.sh
-bsub -o "../outputs/gold_9x9/Markdown/gold_9x9_2.md" -J "gold_9x9_2" -P "-name gold_9x9-2 -hours 24.0 -width 9 -height 9" < submit.sh
-mkdir ../outputs/gold_11x11/
-mkdir ../outputs/gold_11x11/Markdown
-bsub -o "../outputs/gold_11x11/Markdown/gold_11x11_0.md" -J "gold_11x11_0" -P "-name gold_11x11-0 -hours 24.0 -width 11 -height 11" < submit.sh
-bsub -o "../outputs/gold_11x11/Markdown/gold_11x11_1.md" -J "gold_11x11_1" -P "-name gold_11x11-1 -hours 24.0 -width 11 -height 11" < submit.sh
-bsub -o "../outputs/gold_11x11/Markdown/gold_11x11_2.md" -J "gold_11x11_2" -P "-name gold_11x11-2 -hours 24.0 -width 11 -height 11" < submit.sh
+mkdir ../outputs/causal1_9x9/
+mkdir ../outputs/causal1_9x9/Markdown
+bsub -o "../outputs/causal1_9x9/Markdown/causal1_9x9_0.md" -J "causal1_9x9_0" -P "-name causal1_9x9-0 -hours 12.0 -width 9 -height 9 -level Levels.Causal1 -main teleport" < submit.sh
+bsub -o "../outputs/causal1_9x9/Markdown/causal1_9x9_1.md" -J "causal1_9x9_1" -P "-name causal1_9x9-1 -hours 12.0 -width 9 -height 9 -level Levels.Causal1 -main teleport" < submit.sh
+bsub -o "../outputs/causal1_9x9/Markdown/causal1_9x9_2.md" -J "causal1_9x9_2" -P "-name causal1_9x9-2 -hours 12.0 -width 9 -height 9 -level Levels.Causal1 -main teleport" < submit.sh
