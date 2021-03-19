83e1b7eb5546085774d6e60b0339485e9681c9ad
TheDingodile <jakob@3esser.dk>
2021-03-19 17:49:31 +0100

    teser

--- test_simulator.py
+++ test_simulator.py
@@ -13,9 +13,9 @@ def test_simulator():
             env.layers.levelType = Levels.Maze.value  # Fix
             intervention_idx, modified_board = teleporter.pre_process(env)
             buffer = ReplayBuffer(50000, 50)
-            lossboard = [[0 for _ in range(20)] for _ in range(len(env.layers.layers))]
-            lossRD = [0 for _ in range(20)]
-            for i in range(20):
+            lossboard = [[0 for _ in range(10)] for _ in range(len(env.layers.layers))]
+            lossRD = [0 for _ in range(10)]
+            for i in range(10):
                 for frame in loop(env, collector, teleporter=teleporter):
                     modified_board = teleporter.interveen(env.board, intervention_idx, modified_board)
                     actions = mover(modified_board)
@@ -25,18 +25,18 @@ def test_simulator():
                     board_before, board_after, intervention, _, tele_dones, normal_rewards = buffer.sample_data()
                     guessboard, guessRD = simulator.simulate(board_before, intervention)
                     guessboard = guessboard[tele_dones == 0]
-                    limit = (i+1)/60 + 0.2
+                    limit = (i+1)/15 + 0.1
                     guessboard[guessboard > limit] = 1
                     guessboard[guessboard < limit] = 0
                     guessRD[guessRD > limit] = 1
                     guessRD[guessRD < limit] = 0
-                    lossRD[i] += (torch.sum((guessRD - torch.cat((normal_rewards.unsqueeze(1), tele_dones.unsqueeze(1)), dim=1))**2).item())/(guessRD.shape[0] * 10000)
+                    lossRD[i] += (torch.sum((guessRD[:,0] - normal_rewards)**2).item())/(guessRD.shape[0] * 1000)
                     for j in range(len(env.layers.layers)):
-                        lossboard[j][i] += torch.sum((guessboard.reshape(-1, 4, env.layers.height, env.layers.height)[:,j] - ((board_after - board_before)[tele_dones == 0]**2)[:,j])**2).item()/(guessboard.shape[0] * 10000)
-                    if frame % 10000 == 0:
+                        lossboard[j][i] += torch.sum((guessboard.reshape(-1, 4, env.layers.height, env.layers.height)[:,j] - ((board_after - board_before)[tele_dones == 0]**2)[:,j])**2).item()/(guessboard.shape[0] * 1000)
+                    if frame % 1000 == 0:
                         print(i)
                         break
-            x_values = [((x+1)/60 + 0.2) for x in list(range(20))]
+            x_values = [((x+1)/15 + 0.1) for x in list(range(10))]
             for i in range(len(lossboard)):
                 print(lossboard[i])
                 plt.plot(x_values,lossboard[i])
