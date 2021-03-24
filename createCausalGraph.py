96b15c39461735b4999a08ee7ca3b8e7e1f99023
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-03-24 23:47:57 +0100

    Fix

--- createCausalGraph.py
+++ createCausalGraph.py
@@ -13,19 +13,19 @@ with Load("causal2_9x9", num=2) as load:
     for frame in loop(env, collector, teleporter=teleporter):
         intervention_idx, modified_board = teleporter.pre_process(env)
         modified_board = teleporter.interveen(env.board, intervention_idx, modified_board)
-        movers = [torch.sum(torch.sum(modified_board[batch, :-1] * modified_board[batch, -1], dim=1), dim=1).argmax().item() for batch in range(modified_board.shape[0])]
+        movers = array([torch.sum(torch.sum(modified_board[batch, :-1] * modified_board[batch, -1], dim=1), dim=1).argmax().item() for batch in range(modified_board.shape[0])])
         mask = array([layer in convert for layer in movers])
-        print(mask)
         while any(mask):
-            print(mask)
             results = torch.zeros((env.board.shape[0], *(shape := (len(flippables), *env.board.shape[2:]))))
             for layer, x, y in ranges(shape):
                 board = env.board
-                pixel = board[:, convert[layer], x, y]
-                board[:, convert[layer], x, y][pixel == 0], board[:, convert[layer], x, y][pixel == 1], board[:, convert[layer], x, y][pixel == 2] = 2, 0, 1
+                pixel = board[:, layer, x, y]
+                board[:, layer, x, y][pixel == 0], board[:, layer, x, y][pixel == 1], board[:, layer, x, y][pixel == 2] = 2, 0, 1
                 results[:, layer, x, y] = teleporter.net.network(board).max(dim=1)[0]
-            flippers = [convert[v] for v in list(results.max(dim=3)[0].max(dim=2)[0].argmax(dim=1))]
+            flippers = array([convert[v] for v in list(results[mask].max(dim=3)[0].max(dim=2)[0].argmax(dim=1))])
             print(flippers)
+            print(movers[mask])
+            mask[mask] = flippers != movers[mask]
 
         li = [0] * env.layers.batch
         for batch in range(env.layers.batch):
