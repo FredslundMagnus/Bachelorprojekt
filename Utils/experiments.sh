c71cfd1fe93a0d8faca872022a24764b00eb6aa0
TheDingodile <jakob@3esser.dk>
2021-03-24 17:23:59 +0100

    ker

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
-mkdir ../outputs/Rocks_9x9_META_attempt2/
-mkdir ../outputs/Rocks_9x9_META_attempt2/Markdown
-bsub -o "../outputs/Rocks_9x9_META_attempt2/Markdown/Rocks_9x9_META_attempt2_0.md" -J "Rocks_9x9_META_attempt2_0" -P "-name Rocks_9x9_META_attempt2-0 -hours 11.0 -level Levels.Rocks" < submit.sh
-mkdir ../outputs/causal1_9x9_META_attempt2/
-mkdir ../outputs/causal1_9x9_META_attempt2/Markdown
-bsub -o "../outputs/causal1_9x9_META_attempt2/Markdown/causal1_9x9_META_attempt2_0.md" -J "causal1_9x9_META_attempt2_0" -P "-name causal1_9x9_META_attempt2-0 -hours 11.0 -level Levels.Causal1" < submit.sh
-mkdir ../outputs/Rocks_9x9_META_attempt2_highK/
-mkdir ../outputs/Rocks_9x9_META_attempt2_highK/Markdown
-bsub -o "../outputs/Rocks_9x9_META_attempt2_highK/Markdown/Rocks_9x9_META_attempt2_highK_0.md" -J "Rocks_9x9_META_attempt2_highK_0" -P "-name Rocks_9x9_META_attempt2_highK-0 -hours 11.0 -level Levels.Rocks -K 500000.0" < submit.sh
-mkdir ../outputs/causal1_9x9_META_attempt2_highK/
-mkdir ../outputs/causal1_9x9_META_attempt2_highK/Markdown
-bsub -o "../outputs/causal1_9x9_META_attempt2_highK/Markdown/causal1_9x9_META_attempt2_highK_0.md" -J "causal1_9x9_META_attempt2_highK_0" -P "-name causal1_9x9_META_attempt2_highK-0 -hours 11.0 -level Levels.Causal1 -K 500000.0" < submit.sh
-mkdir ../outputs/causal2_9x9_META_attempt2/
-mkdir ../outputs/causal2_9x9_META_attempt2/Markdown
-bsub -o "../outputs/causal2_9x9_META_attempt2/Markdown/causal2_9x9_META_attempt2_0.md" -J "causal2_9x9_META_attempt2_0" -P "-name causal2_9x9_META_attempt2-0 -hours 11.0 -level Levels.Causal2 -layer_Diamond1 True -layer_Diamond2 True -layer_Diamond3 True -layer_Diamond4 True -layer_Dirt False -layer_Rock False -layer_Gold False" < submit.sh
+mkdir ../outputs/test_METAk100000/
+mkdir ../outputs/test_METAk100000/Markdown
+bsub -o "../outputs/test_METAk100000/Markdown/test_METAk100000_0.md" -J "test_METAk100000_0" -P "-name test_METAk100000-0 -hours 4.0 -level Levels.Causal1 -K 100000.0" < submit.sh
+mkdir ../outputs/test_METAk500000/
+mkdir ../outputs/test_METAk500000/Markdown
+bsub -o "../outputs/test_METAk500000/Markdown/test_METAk500000_0.md" -J "test_METAk500000_0" -P "-name test_METAk500000-0 -hours 4.0 -level Levels.Causal1 -K 500000.0" < submit.sh
+mkdir ../outputs/test_METAk1000000/
+mkdir ../outputs/test_METAk1000000/Markdown
+bsub -o "../outputs/test_METAk1000000/Markdown/test_METAk1000000_0.md" -J "test_METAk1000000_0" -P "-name test_METAk1000000-0 -hours 4.0 -level Levels.Causal1 -K 1000000.0" < submit.sh
