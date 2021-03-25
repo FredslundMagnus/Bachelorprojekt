82406693f2ca0875a63d0da94528a6223bcb93e5
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-03-25 10:07:36 +0100

    Fix

--- causalityTest.py
+++ causalityTest.py
@@ -12,7 +12,6 @@ with Load("causal2_9x9", num=2) as load:
     counter = {layer: 0 for layer in env.layers.types}
     counter2 = {layer: 0 for layer in env.layers.types}
     counter3 = {(layer1, layer2): 0 for layer1 in env.layers.types for layer2 in env.layers.types}
-    rounds = 0
     for frame in loop(env, collector, teleporter=teleporter):
         intervention_idx, modified_board = teleporter.pre_process(env)
         results = torch.zeros(*(shape := env.board.shape))
@@ -24,15 +23,12 @@ with Load("causal2_9x9", num=2) as load:
         flippers = [int(v) for v in list(results.max(dim=3)[0].max(dim=2)[0].argmax(dim=1))]
         for i in flippers:
             counter[env.layers.types[i]] += 1
-        print(results.max(dim=3)[0].max(dim=2)[0].argsort(dim=1))
-        # print(counter)
         modified_board = teleporter.interveen(env.board, intervention_idx, modified_board)
         actions = mover(modified_board)
         observations, rewards, dones, info = env.step(actions)
         movers = [torch.sum(torch.sum(modified_board[batch, :-1] * modified_board[batch, -1], dim=1), dim=1).argmax().item() for batch in range(modified_board.shape[0])]
         for i in movers:
             counter2[env.layers.types[i]] += 1
-        # print(counter2)
         counter2 = {layer: 0 for layer in env.layers.types}
         for å in range(100):
             for i in range(len(env.layers.types)):
@@ -41,9 +37,8 @@ with Load("causal2_9x9", num=2) as load:
             counter3[(env.layers.types[flipper], env.layers.types[mover_])] += 1
 
         restart(env)
-        rounds += 1
-        print(rounds, end=",")
-        if rounds == 200:
+        print(frame, end=",")
+        if frame == 200:
             print("")
             break
 
--- createCausalGraph.py
+++ createCausalGraph.py
@@ -14,7 +14,6 @@ with Load("causal2_9x9", num=2) as load:
     convert = [env.layers.types.index(layer) for layer in flippables]
     d = {}
     for frame in loop(env, collector, teleporter=teleporter):
-        restart(env)
         intervention_idx, modified_board = teleporter.pre_process(env)
         modified_board = teleporter.interveen(env.board, intervention_idx, modified_board)
         movers = array([torch.sum(torch.sum(modified_board[batch, :-1] * modified_board[batch, -1], dim=1), dim=1).argmax().item() for batch in range(modified_board.shape[0])])
@@ -22,15 +21,12 @@ with Load("causal2_9x9", num=2) as load:
         results = torch.zeros((env.board.shape[0], *(shape := (len(flippables), *env.board.shape[2:]))))
         for layer, x, y in ranges(shape):
             board = env.board
-            pixel = board[:, layer, x, y]
-            board[:, layer, x, y][pixel == 0], board[:, layer, x, y][pixel == 1], board[:, layer, x, y][pixel == 2] = 2, 0, 1
+            pixel = board[:, convert[layer], x, y]
+            board[:, convert[layer], x, y][pixel == 0], board[:, convert[layer], x, y][pixel == 1], board[:, convert[layer], x, y][pixel == 2] = 2, 0, 1
             results[:, layer, x, y] = teleporter.net.network(board).max(dim=1)[0]
         flip = list(results[mask].max(dim=3)[0].max(dim=2)[0].argsort(dim=1, descending=True))
         paths = [[] for _ in range(len(flip))]
         for i in range(len(flippables)):
-            print(results[mask].max(dim=3)[0].max(dim=2)[0])
-            print(flip)
-            quit()
             flippers = [convert[v[i]] for v in flip]
             for i, f in enumerate(flippers):
                 paths[i].append(f)
@@ -43,4 +39,23 @@ with Load("causal2_9x9", num=2) as load:
                     d[tuple(p)] = 1
             paths = [v for v, a in zip(paths, alive) if a]
             mask[mask] = alive
-        print(d)
+        # for k in list(sorted(d, key=d.get, reverse=True))[:10]:
+        #     print(k, d[k], end=" : ")
+        # print("")
+        restart(env)
+        print(frame, end=",")
+        if frame == 100:
+            print("")
+            break
+    counter_first = {k: 0 for k in convert}
+    for path in d:
+        counter_first[path[0]] += d[path]
+
+    counter_total = {k: 0 for k in convert}
+    for path in d:
+        for k in counter_total:
+            if k in path:
+                counter_total[k] += d[path]
+
+    for layer, k in zip(flippables, convert):
+        print(f"{layer.name}: {100*counter_first[k]//counter_total[k]}%")
