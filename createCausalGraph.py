28c7448d3785dcb5868ca8d5173fd29154534f40
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-03-28 12:03:45 +0200

    Fix

--- createCausalGraph.py
+++ createCausalGraph.py
@@ -10,7 +10,8 @@ from threading import currentThread
 
 environments = {
     Levels.Causal3: ["causal3_9x9_20hours", 2, [LayerType.Gold, LayerType.Dirt, LayerType.Bluedoor, LayerType.Bluekeys, LayerType.Reddoor, LayerType.Redkeys]],
-    Levels.Causal2: ["causal2_9x9_0.3", 0, [LayerType.Diamond1, LayerType.Diamond2, LayerType.Diamond3, LayerType.Diamond4]]
+    Levels.Causal2: ["causal2_9x9_0.3", 0, [LayerType.Diamond1, LayerType.Diamond2, LayerType.Diamond3, LayerType.Diamond4]],
+    Levels.Causal1: ["causal1_good_24h", 0, [LayerType.Gold, LayerType.Keys, LayerType.Door]]
 }
 
 environment = environments[Levels.Causal2]
