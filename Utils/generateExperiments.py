c2e2e8c7e7f823ddd4f138236b4d13943df4f442
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-04-07 16:18:59 +0200

    Fix

--- Utils/generateExperiments.py
+++ Utils/generateExperiments.py
@@ -146,10 +146,12 @@ def genExperiments(name, n=1, **params):
 # genExperiments(f"causal3_CF_convert3_2", n=1, hours=24, level=Levels.Causal3, main=CFagent, CF_convert=3)
 # genExperiments(f"causal3_CF_convert4_2", n=1, hours=24, level=Levels.Causal3, main=CFagent, CF_convert=4)
 # genExperiments(f"causal3_CF_convert5_2", n=1, hours=24, level=Levels.Causal3, main=CFagent, CF_convert=5)
+# genExperiments(f"causal1_demo", n=2, hours=16, level=Levels.Causal1, main=teleport)
 # genExperiments(f"causal2_demo", n=2, hours=16, level=Levels.Causal2, main=teleport)
+# genExperiments(f"causal3_demo", n=2, hours=16, level=Levels.Causal3, main=teleport)
 # genExperiments(f"causal5_demo", n=2, hours=16, level=Levels.Causal5, main=teleport)
 # genExperiments(f"causal6_demo", n=2, hours=16, level=Levels.Causal6, main=teleport)
-# genExperiments(f"causal1_demo", n=2, hours=16, level=Levels.Causal1, main=teleport)
+# genExperiments(f"causal7_demo", n=2, hours=16, level=Levels.Causal7, main=teleport)
 # genExperiments(f"cococonuts_CF_conver2", n=1, hours=24, level=Levels.Coconuts, main=CFagent, CF_convert=2)
 # genExperiments(f"cococonuts_CF_conver3", n=1, hours=24, level=Levels.Coconuts, main=CFagent, CF_convert=3)
 # genExperiments(f"cococonuts_CF_conver4", n=1, hours=24, level=Levels.Coconuts, main=CFagent, CF_convert=4)
@@ -157,7 +159,5 @@ def genExperiments(name, n=1, **params):
 # genExperiments(f"cococonuts_CF_conver9", n=1, hours=24, level=Levels.Coconuts, main=CFagent, CF_convert=9)
 # genExperiments(f"cococonuts_teleport", n=1, hours=24, level=Levels.Coconuts, main=teleport)
 
-
-genExperiments(f"causal3_demo", n=2, hours=16, level=Levels.Causal3, main=teleport)
 
 file.close()
