d7f665bb8908196e9076aaa8b8f407dcac31486b
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-04-12 17:13:51 +0200

    Fix

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-mkdir ../outputs/causal2_online_var_0.5_test/
-mkdir ../outputs/causal2_online_var_0.5_test/Markdown
-bsub -o "../outputs/causal2_online_var_0.5_test/Markdown/causal2_online_var_0.5_test_0.md" -J "causal2_online_var_0.5_test_0" -P "-name causal2_online_var_0.5_test-0 -hours 5.0 -level Levels.Causal2 -main graphTrain -K1 200000.0 -K2 100000.0 -softmax_cap 0.0 -failed_actions_chance 0.5" < submit.sh
-mkdir ../outputs/causal2_online_var_0.5/
-mkdir ../outputs/causal2_online_var_0.5/Markdown
-bsub -o "../outputs/causal2_online_var_0.5/Markdown/causal2_online_var_0.5_0.md" -J "causal2_online_var_0.5_0" -P "-name causal2_online_var_0.5-0 -hours 12.0 -level Levels.Causal2 -main graphTrain -K1 200000.0 -K2 100000.0 -softmax_cap 0.0 -failed_actions_chance 0.5" < submit.sh
-mkdir ../outputs/causal5_online_var_0.5/
-mkdir ../outputs/causal5_online_var_0.5/Markdown
-bsub -o "../outputs/causal5_online_var_0.5/Markdown/causal5_online_var_0.5_0.md" -J "causal5_online_var_0.5_0" -P "-name causal5_online_var_0.5-0 -hours 12.0 -level Levels.Causal5 -main graphTrain -K1 200000.0 -K2 100000.0 -softmax_cap 0.0 -failed_actions_chance 0.5" < submit.sh
-mkdir ../outputs/causal6_online_var_0.5/
-mkdir ../outputs/causal6_online_var_0.5/Markdown
-bsub -o "../outputs/causal6_online_var_0.5/Markdown/causal6_online_var_0.5_0.md" -J "causal6_online_var_0.5_0" -P "-name causal6_online_var_0.5-0 -hours 12.0 -level Levels.Causal6 -main graphTrain -K1 200000.0 -K2 100000.0 -softmax_cap 0.0 -failed_actions_chance 0.5" < submit.sh
 mkdir ../outputs/causal7_online_var_0.5/
 mkdir ../outputs/causal7_online_var_0.5/Markdown
 bsub -o "../outputs/causal7_online_var_0.5/Markdown/causal7_online_var_0.5_0.md" -J "causal7_online_var_0.5_0" -P "-name causal7_online_var_0.5-0 -hours 12.0 -level Levels.Causal7 -main graphTrain -K1 200000.0 -K2 100000.0 -softmax_cap 0.0 -failed_actions_chance 0.5" < submit.sh
--- Utils/generateExperiments.py
+++ Utils/generateExperiments.py
@@ -159,10 +159,10 @@ def genExperiments(name, n=1, **params):
 # genExperiments(f"cococonuts_teleport", n=1, hours=24, level=Levels.Coconuts, main=teleport)
 
 
-genExperiments(f"causal2_online_var_0.5_test", n=1, hours=5, level=Levels.Causal2, main=graphTrain, K1=200000, K2=100000, softmax_cap=0.0, failed_actions_chance=0.5)
-genExperiments(f"causal2_online_var_0.5", n=1, hours=12, level=Levels.Causal2, main=graphTrain, K1=200000, K2=100000, softmax_cap=0.0, failed_actions_chance=0.5)
-genExperiments(f"causal5_online_var_0.5", n=1, hours=12, level=Levels.Causal5, main=graphTrain, K1=200000, K2=100000, softmax_cap=0.0, failed_actions_chance=0.5)
-genExperiments(f"causal6_online_var_0.5", n=1, hours=12, level=Levels.Causal6, main=graphTrain, K1=200000, K2=100000, softmax_cap=0.0, failed_actions_chance=0.5)
+# genExperiments(f"causal2_online_var_0.5_test", n=1, hours=5, level=Levels.Causal2, main=graphTrain, K1=200000, K2=100000, softmax_cap=0.0, failed_actions_chance=0.5)
+# genExperiments(f"causal2_online_var_0.5", n=1, hours=12, level=Levels.Causal2, main=graphTrain, K1=200000, K2=100000, softmax_cap=0.0, failed_actions_chance=0.5)
+# genExperiments(f"causal5_online_var_0.5", n=1, hours=12, level=Levels.Causal5, main=graphTrain, K1=200000, K2=100000, softmax_cap=0.0, failed_actions_chance=0.5)
+# genExperiments(f"causal6_online_var_0.5", n=1, hours=12, level=Levels.Causal6, main=graphTrain, K1=200000, K2=100000, softmax_cap=0.0, failed_actions_chance=0.5)
 genExperiments(f"causal7_online_var_0.5", n=1, hours=12, level=Levels.Causal7, main=graphTrain, K1=200000, K2=100000, softmax_cap=0.0, failed_actions_chance=0.5)
 
 # genExperiments(f"MONTEST_CF_5c1", n=2, hours=22, level=Levels.MonsterLevel, main=CFagent, CF_convert=5, Counterfacts=1)
