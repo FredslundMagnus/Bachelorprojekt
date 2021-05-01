52ce1463388cae713794fd116a3105669edc929a
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-05-01 14:43:37 +0200

    Fix

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mkdir ../outputs/Test_CPU_3/
-mkdir ../outputs/Test_CPU_3/Markdown
-bsub -o "../outputs/Test_CPU_3/Markdown/Test_CPU_3_0.md" -J "Test_CPU_3_0" -env MYARGS="-name Test_CPU_3-0 -level Levels.Causal3 -main option_critic_run -batch 25" < submit_cpu.sh
+mkdir ../outputs/Test_CPU_4/
+mkdir ../outputs/Test_CPU_4/Markdown
+bsub -o "../outputs/Test_CPU_4/Markdown/Test_CPU_4_0.md" -J "Test_CPU_4_0" -env MYARGS="-name Test_CPU_4-0 -level Levels.Causal3 -main option_critic_run -batch 25" < submit_cpu.sh
