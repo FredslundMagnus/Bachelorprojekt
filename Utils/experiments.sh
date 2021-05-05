d39fa9dcce5027ab3be11b5170297f9c3e338706
TheDingodile <jakob@3esser.dk>
2021-05-06 00:27:59 +0200

    3interventions

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-mkdir ../outputs/Causal3_Conver4/
-mkdir ../outputs/Causal3_Conver4/Markdown
-bsub -o "../outputs/Causal3_Conver4/Markdown/Causal3_Conver4_0.md" -J "Causal3_Conver4_0" -env MYARGS="-name Causal3_Conver4-0 -hours 24.0 -level Levels.Causal3 -main CFagent -CF_convert 4 -TopN 3" < submit_gpu.sh
-bsub -o "../outputs/Causal3_Conver4/Markdown/Causal3_Conver4_1.md" -J "Causal3_Conver4_1" -env MYARGS="-name Causal3_Conver4-1 -hours 24.0 -level Levels.Causal3 -main CFagent -CF_convert 4 -TopN 3" < submit_gpu.sh
-bsub -o "../outputs/Causal3_Conver4/Markdown/Causal3_Conver4_2.md" -J "Causal3_Conver4_2" -env MYARGS="-name Causal3_Conver4-2 -hours 24.0 -level Levels.Causal3 -main CFagent -CF_convert 4 -TopN 3" < submit_gpu.sh
+mkdir ../outputs/Causal4_Conver4_3counterfacts/
+mkdir ../outputs/Causal4_Conver4_3counterfacts/Markdown
+bsub -o "../outputs/Causal4_Conver4_3counterfacts/Markdown/Causal4_Conver4_3counterfacts_0.md" -J "Causal4_Conver4_3counterfacts_0" -env MYARGS="-name Causal4_Conver4_3counterfacts-0 -hours 24.0 -level Levels.Causal4 -main CFagent -CF_convert 4 -TopN 5 -Counterfacts 3" < submit_gpu.sh
+bsub -o "../outputs/Causal4_Conver4_3counterfacts/Markdown/Causal4_Conver4_3counterfacts_1.md" -J "Causal4_Conver4_3counterfacts_1" -env MYARGS="-name Causal4_Conver4_3counterfacts-1 -hours 24.0 -level Levels.Causal4 -main CFagent -CF_convert 4 -TopN 5 -Counterfacts 3" < submit_gpu.sh
+bsub -o "../outputs/Causal4_Conver4_3counterfacts/Markdown/Causal4_Conver4_3counterfacts_2.md" -J "Causal4_Conver4_3counterfacts_2" -env MYARGS="-name Causal4_Conver4_3counterfacts-2 -hours 24.0 -level Levels.Causal4 -main CFagent -CF_convert 4 -TopN 5 -Counterfacts 3" < submit_gpu.sh
